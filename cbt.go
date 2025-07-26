5d4f2770f97e1e301ee334ff61188e9e16827928
Alex O'Neill <me@aoneill.me>
2025-07-26 14:42:43 -0400

    feat: samplerowkeys

--- cbt.go
+++ cbt.go
@@ -821,6 +821,13 @@ var commands = []struct {
 		Usage:    "cbt waitforreplication <table-id>\n",
 		Required: ProjectAndInstanceRequired,
 	},
+	{
+		Name:     "samplerowkeys",
+		Desc:     "Sample the row keys in a table",
+		do:       doSampleRowKeys,
+		Usage:    "cbt samplerowkeys <table-id>\n",
+		Required: ProjectAndInstanceRequired,
+	},
 }
 
 func doNotices(ctx context.Context, args ...string) {
@@ -1770,6 +1777,21 @@ func doWaitForReplicaiton(ctx context.Context, args ...string) {
 	fmt.Printf("Waiting for all writes up to %s to be replicated.\n", time.Now().Format("2006/01/02-15:04:05"))
 	if err := getAdminClient().WaitForReplication(ctx, table); err != nil {
 		log.Fatalf("Waiting for replication: %v", err)
+	}
+}
+
+func doSampleRowKeys(ctx context.Context, args ...string) {
+	if len(args) != 1 {
+		log.Fatalf("usage: cbt samplerowkeys <table>")
+	}
+
+	tbl := getClient(bigtable.ClientConfig{}).OpenTable(args[0])
+	keys, err := tbl.SampleRowKeys(ctx)
+	if err != nil {
+		log.Fatalf("Could not sample row keys: %v", err)
+	}
+	for k := range keys {
+		fmt.Println(k)
 	}
 }
 
